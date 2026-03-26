600b6075eeac5a0f21160a5f743e99eacd48150f
vboxuser <ronielsonoliveira@live.com>
2026-03-26 11:13:53 -0300

    first commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,24 +1,24 @@
+require("dotenv").config();
+
 module.exports = {
-  production: {
-    dialect: "mariadb",
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
+  dialect: "mariadb",
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
 
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-      createdAt: "created_at",
-      updatedAt: "updated_at",
-    },
-
-    dialectOptions: {
-      allowPublicKeyRetrieval: true,
-    },
-
-    timezone: "America/Sao_Paulo",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
   },
+
+  dialectOptions: {
+    allowPublicKeyRetrieval: true,
+  },
+
+  timezone: "America/Sao_Paulo",
 };
