7ae861e353cf959a6a6636a6d045b6a365e5c20d
vboxuser <ronielsonoliveira@live.com>
2026-07-08 13:01:30 -0300

    router changes

--- src/controllers/AlunoTreinoController.js
+++ src/controllers/AlunoTreinoController.js
@@ -3,7 +3,44 @@ import Exercicio from "../models/Exercicio";
 import FotoExercicio from "../models/FotoExercicio";
 
 class AlunoTreinoController {
+  // Aluno logado
   async index(req, res) {
+    try {
+      const alunoId = req.userId;
+
+      const treinos = await Treino.findAll({
+        where: {
+          aluno_id: alunoId,
+        },
+        attributes: ["id", "nome", "descricao"],
+        include: [
+          {
+            model: Exercicio,
+            attributes: ["id", "nome", "descricao"],
+            through: {
+              attributes: ["numerodeSeries", "numerodeRepeticoes"],
+            },
+            include: [
+              {
+                model: FotoExercicio,
+                attributes: ["id", "filename", "url"],
+              },
+            ],
+          },
+        ],
+        order: [["id", "DESC"]],
+      });
+
+      return res.json(treinos);
+    } catch (e) {
+      return res.status(400).json({
+        errors: [e.message],
+      });
+    }
+  }
+
+  // Personal/Admin
+  async show(req, res) {
     try {
       const { alunoId } = req.params;
 
